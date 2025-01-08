e9efecce4859115323d959ebfdaa2538e83382f2
AleksKraken <xxxaleks85xxx@gmail.com>
2025-01-08 21:00:38 +0200

    t3

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+// src/components/Product.jsx
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    sourcemap: true,
-  }
-});
+export default function Product() {
+	const price = 999;
+	const imgUrl = "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640";
+
+  return (
+    <>
+      <h2>Tacos</h2>
+	  <img src={imgUrl} alt="Tacos With Lime" width="640" />
+      <p>Price: {price} credits</p>
+    </>
+  );
+};
